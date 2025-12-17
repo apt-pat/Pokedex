4afc9cceb3588a81d6c21a2e8284b17bcedd25fc
Patrick C. C. <126633888+apt-pat@users.noreply.github.com>
2025-12-17 08:55:38 -0300

    Update pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -2,20 +2,22 @@ import requests
 import pandas as pd
 import urllib3
 
+
 urllib3.disable_warnings()
 
 minha_coleta = []
 
-print("Iniciando coleta TURBINADA (Dados + Imagem HD + Descrição)...")
-print("Isso vai demorar um pouco mais pois faremos 2 requisições por Pokémon.")
-
 for i in range(1, 152):
+
     url_padrao = f'https://pokeapi.co/api/v2/pokemon/{i}'
+
     url_especie = f'https://pokeapi.co/api/v2/pokemon-species/{i}'
 
     try:
+
         r_padrao = requests.get(url_padrao, verify=False)
         data = r_padrao.json()
+
 
         r_especie = requests.get(url_especie, verify=False)
         data_especie = r_especie.json()
@@ -25,18 +27,21 @@ for i in range(1, 152):
 
         for entrada in data_especie['flavor_text_entries']:
             if entrada['language']['name'] == 'en':
+
                 descricao_texto = entrada['flavor_text'].replace('\n', ' ').replace('\f', ' ')
-                break  
+                break
+
 
         if len(data['types']) > 1:
             tipo_2 = data['types'][1]['type']['name']
         else:
-            tipo_2 = 'puro'  
+            tipo_2 = 'puro'
+
 
         pokemon_dict = {
             'id': data['id'],
             'nome': data['name'],
-            'descricao': descricao_texto,  
+            'descricao': descricao_texto,
             'tipo_1': data['types'][0]['type']['name'],
             'tipo_2': tipo_2,
             'habilidade': data['abilities'][0]['ability']['name'],
@@ -48,6 +53,7 @@ for i in range(1, 152):
             'ataque_especial': data['stats'][3]['base_stat'],
             'defesa_especial': data['stats'][4]['base_stat'],
             'velocidade': data['stats'][5]['base_stat'],
+
             'foto_url': data['sprites']['other']['official-artwork']['front_default']
         }
 
@@ -59,9 +65,11 @@ for i in range(1, 152):
     except Exception as e:
         print(f"Erro no ID {i}: {e}")
 
+
 df = pd.DataFrame(minha_coleta)
+
 
 df.to_csv('pokedex_final_completa.csv', index=False)
 
 print("\nConcluído! Arquivo 'pokedex_final_completa.csv' gerado.")
-print(df[['nome', 'descricao']].head())  
+print(df[['nome', 'descricao']].head())
